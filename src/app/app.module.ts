2071d311052a1571663ec15bec9744db80657dd4
Ruslan Nazarov <Ruslan93Nazarov@gmail.com>
2023-08-21 12:55:20 +0300

    Start tiscetSalesMastar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorizationComponent } from './pages/auth/authorization/authorization.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AuthorizationComponent, AuthComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, InputTextModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
